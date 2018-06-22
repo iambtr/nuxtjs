df76fd0427cca563551aadcaf513ec302083481f
wangrong <807393731@qq.com>
2018-06-22 09:07:00 +0800

    _nuxt2nuxt

--- _nuxt/server-bundle.js
+++ _nuxt/server-bundle.js
@@ -61,7 +61,7 @@ module.exports =
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/_nuxt/";
+/******/ 	__webpack_require__.p = "/nuxt/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 196);
